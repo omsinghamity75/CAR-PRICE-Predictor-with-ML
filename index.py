933f76820210430cc639bec13361b1eb20871d02
OM SINGH <omsingh122003@gmail.com>
2026-01-26 23:16:17 +0530

    Remove data cleaning operations from index.py
    Removed data cleaning steps for the car DataFrame.

--- index.py
+++ index.py
@@ -13,23 +13,3 @@ car.head()
 car.shape
 car.info()
 
-backup=car.copy()
-
-car=car[car['year'].str.isnumeric()]
-
-car['year']=car['year'].astype(int)
-
-car=car[car['Price']!='Ask For Price']
-
-car['Price']=car['Price'].str.replace(',','').astype(int)
-
-car['kms_driven']=car['kms_driven'].str.split().str.get(0).str.replace(',','')
-
-car=car[car['kms_driven'].str.isnumeric()]
-car['kms_driven']=car['kms_driven'].astype(int)
-
-car=car[~car['fuel_type'].isna()]
-car.shape
-
-car['name']=car['name'].str.split().str.slice(start=0,stop=3).str.join(' ')
-car=car.reset_index(drop=True)
