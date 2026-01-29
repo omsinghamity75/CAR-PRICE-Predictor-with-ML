c525a0d947aae385874d534fec1e272e2fbffb2a
OM SINGH <omsingh122003@gmail.com>
2026-01-29 16:49:42 +0530

    Add boxplot visualization for company vs price

---
+++ Checking relationship of Company with Price.py
@@ -0,0 +1,7 @@
+car['company'].unique()
+
+import seaborn as sns
+plt.subplots(figsize=(15,7))
+ax=sns.boxplot(x='company',y='Price',data=car)
+ax.set_xticklabels(ax.get_xticklabels(),rotation=40,ha='right')
+plt.show()
